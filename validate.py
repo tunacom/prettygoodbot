6dbd6f620c1ca7b0a3c5c5303f20199630b1c621
tunacom <tuemacom@gmail.com>
2020-03-13 20:29:40 -0700

    Add more stuff.

---
+++ validate.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config.json')
+ICONS_DIR = os.path.join(os.path.dirname(__file__), 'icons')
+
+
+def validate_entity(e):
+  assert 'name' in e
+  assert 'icon' in e
+  assert os.path.exists(os.path.join(ICONS_DIR, e['icon']))
+
+
+def main():
+  data = json.load(open(CONFIG_PATH))
+  for k, v in data.items():
+    if k == 'default':
+      for e in v:
+        validate_entity(e)
+    else:
+      validate_entity(v)
+
+
+if __name__ == '__main__':
+  main()
+  
