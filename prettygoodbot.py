5c8dbf6a98e4348071f22c4405666561173433c7
tunacom <tunacom@users.noreply.github.com>
2022-08-23 09:44:37 -0400

    Update formatting and discord.py version

--- prettygoodbot.py
+++ prettygoodbot.py
@@ -10,47 +10,49 @@ TOKEN_PATH = os.path.join(os.path.dirname(__file__), 'discord_token')
 
 
 def get_name_and_icon():
-  """Return the configured server name and icon for this day of the week."""
-  config = json.load(open(CONFIG_PATH))
-  day = str(datetime.datetime.today().weekday())
+    """Return the configured server name and icon for this day of the week."""
+    config = json.load(open(CONFIG_PATH))
+    day = str(datetime.datetime.today().weekday())
 
-  if day in config:
-    return config[day]['name'], config[day]['icon']
+    if day in config:
+        return config[day]['name'], config[day]['icon']
 
-  default = config['default']
-  entry = random.choice(default)
-  return entry['name'], entry['icon']
+    default = config['default']
+    entry = random.choice(default)
+    return entry['name'], entry['icon']
 
 
 class PrettyGoodClient(discord.Client):
-  """Client for the pretty good bot."""
+    """Client for the pretty good bot."""
 
-  async def on_ready(self):
-    guilds = self.fetch_guilds()
-    async for guild in guilds:
-      # TODO(tuna): discord.py contains a type check that breaks if the server
-      # has no verification level set. Explicitly set one to work around it.
-      # Remove this once it's fixed upstream.
-      verification_level = guild.verification_level
-      if verification_level is None:
-        verification_level = discord.VerificationLevel.none
+    async def on_ready(self):
+        guilds = self.fetch_guilds()
+        async for guild in guilds:
+            # TODO(tuna): discord.py contains a type check that breaks if the
+            # server has no verification level set. Explicitly set one to work
+            # around it. Remove this once it's fixed upstream.
+            verification_level = guild.verification_level
+            if verification_level is None:
+                verification_level = discord.VerificationLevel.none
 
-      name, icon = get_name_and_icon()
-      icon_data = open(os.path.join(ICONS_DIR, icon), 'rb').read()
-      await guild.edit(
-          name=name, icon=icon_data, verification_level=verification_level)
+            name, icon = get_name_and_icon()
+            icon_data = open(os.path.join(ICONS_DIR, icon), 'rb').read()
+            await guild.edit(name=name,
+                             icon=icon_data,
+                             verification_level=verification_level)
 
-    await self.close()
+        await self.close()
 
 
 def main():
-  """Run the client."""
-  with open(TOKEN_PATH) as handle:
-    token = handle.read()
+    """Run the client."""
+    with open(TOKEN_PATH) as handle:
+        token = handle.read().strip()
 
-  client = PrettyGoodClient()
-  client.run(token)
+    intents = discord.Intents.default()
+    client = PrettyGoodClient(intents=intents)
+    client.run(token)
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- validate.py
+++ validate.py
@@ -6,22 +6,21 @@ ICONS_DIR = os.path.join(os.path.dirname(__file__), 'icons')
 
 
 def validate_entity(e):
-  assert 'name' in e
-  assert 'icon' in e
-  assert os.path.exists(os.path.join(ICONS_DIR, e['icon']))
-  assert e['icon'].endswith('.png')
+    assert 'name' in e
+    assert 'icon' in e
+    assert os.path.exists(os.path.join(ICONS_DIR, e['icon']))
+    assert e['icon'].endswith('.png')
 
 
 def main():
-  data = json.load(open(CONFIG_PATH))
-  for k, v in data.items():
-    if k == 'default':
-      for e in v:
-        validate_entity(e)
-    else:
-      validate_entity(v)
+    data = json.load(open(CONFIG_PATH))
+    for k, v in data.items():
+        if k == 'default':
+            for e in v:
+                validate_entity(e)
+        else:
+            validate_entity(v)
 
 
 if __name__ == '__main__':
-  main()
-  
+    main()
